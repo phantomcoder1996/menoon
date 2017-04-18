2a4ac2fe038ee95e9e17696a84833eb2f167d30c
Reem <macbookpro@MacBooks-MBP.Home>
2017-04-18 20:08:53 +0200

    added default value to reviewed field

---
+++ database/migrations/2017_04_18_180449_modify_rev_field.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyRevField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('feedback', function (Blueprint $table) {
+            //
+            $table->boolean('reviewed')->default(0)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('feedback', function (Blueprint $table) {
+            //
+        });
+    }
+}
