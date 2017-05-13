245edc7bafb74b39dd0765dec105c6574df3ce82
Reem <macbookpro@MacBooks-MBP.Home>
2017-05-13 22:03:44 +0200

    added resources/views/admin

---
+++ resources/views/includes/adminHeader.blade.php
@@ -0,0 +1,11 @@
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Admin view</title>
+
+     <!-- GOOGLE FONTS-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+   {!! Html::style( "css/bootstrap.min.css")!!}
+   {!! Html::style( "assets/css/font-awesome.css")!!}
+   {!! Html::style( "assets/css/custom.css")!!}
+      {!! Html::style( "css/menoon.css")!!}
---
+++ resources/views/layouts/adminLayout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+@include('includes.adminHeader')
+</head>
+<body>
+     
+           
+          
+@yield('content')
+          
+
+
+    
+   <footer>
+
+@include('includes.adminFooter')
+
+@yield('scripts')
+   </footer>
+</body>
+</html>
